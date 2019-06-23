a2118161483f1a8f1af1e3b3f05ef80f05f4dfb8
Robin Maenhaut <robin.maenhaut.coder@gmail.com>
2019-06-23 17:12:33 +0200

    Add reactive and non-reactive example of filtering a list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StarshipsOverviewComponent } from './starships/components/starships-overview/starships-overview.component';
 import { StartshipsDetailComponent } from './starships/components/startships-detail/startships-detail.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { StarshipsOverviewDirtyComponent } from './starships/components/starships-overview-dirty/starships-overview-dirty.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StarshipsOverviewComponent,
-    StartshipsDetailComponent
+    StartshipsDetailComponent,
+    StarshipsOverviewDirtyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
