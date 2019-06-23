9fced28f06d817fd94df3977e419040ad308b665
Robin Maenhaut <robin.maenhaut.coder@gmail.com>
2019-06-23 17:12:40 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StarshipsOverviewComponent } from './starships/components/starships-overview/starships-overview.component';
+import { StarshipsOverviewDirtyComponent } from './starships/components/starships-overview-dirty/starships-overview-dirty.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'starships', component: StarshipsOverviewComponent },
+  { path: 'starships-dirty', component: StarshipsOverviewDirtyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
