5be6d605ef29326c538c431fc5a2216515a90a57
Robin Maenhaut <robin.maenhaut.coder@gmail.com>
2019-06-23 21:09:28 +0200

    Custom rxjs operator example

---
+++ src/app/observables-operators-custom/operator-uppercase/operator-uppercase.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { of, Observable, OperatorFunction } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-operator-uppercase',
+  templateUrl: '{{ uppercaseName$ | async }}'
+})
+export class OperatorUppercaseComponent {
+
+  name$ = of('Robin Maenhaut');
+  uppercaseName$ = this.name$.pipe(uppercase());
+}
+
+/**
+ * Custom pipeable operator using existing operators
+ */
+export function uppercase(): OperatorFunction<string, string> {
+  return function uppercaseOperation(source: Observable<string>): Observable<string> {
+    return source.pipe(map((value: string) => value.toUpperCase()));
+  };
+}
+
+/**
+ * You can also use a lambda like so
+ */
+export const uppercaseShorter = (): OperatorFunction<string, string> => {
+  return (source: Observable<string>) => source.pipe(map((value: string) => value.toUpperCase()));
+};
+
+/**
+ * And since pipeable operators return functions, you can further simplify like so
+ */
+export const uppercaseShortest = () => map((value: string) => value.toUpperCase());
+
